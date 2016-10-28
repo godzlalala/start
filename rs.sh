277a38f510ce0e8a8322984fad57bb908a580128
godzlalala <admin@skvm.win>
2016-10-28 11:56:46 +0800

    Update rs.sh

--- rs.sh
+++ rs.sh
@@ -10,7 +10,7 @@ function restart(){
 function status(){
 /serverspeeder/bin/serverSpeeder.sh status
 }
-function peizhi(){
+function config(){
 checkos
  if [ "$OS" == 'CentOS' ]; then
   vi /serverspeeder/etc/config
@@ -22,22 +22,10 @@ checkos
 function checkos(){
 	if [ -f /etc/redhat-release ];then
         OS=CentOS
-		echo "
-		
-		
-		Your OS is CentOS"
     elif [ ! -z "`cat /etc/issue | grep bian`" ];then
         OS=Debian
-		echo "
-		
-		
-		Your OS is Debian"
     elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
         OS=Ubuntu
-		echo "
-		
-		
-		Your OS is Ubuntu"
     else
         echo "Not support OS, Please reinstall OS and retry!"
         exit 1
@@ -54,13 +42,11 @@ elif [ $1 = "restart" ]; then
  restart
  elif [ $1 = "status" ]; then
  status
-  elif [ $1 = "peizhi" ]; then
- peizhi
+  elif [ $1 = "config" ]; then
+ config
 else 
 	echo "
-	print error bash start 
-	bash start lnmp to install lnmp
-	bash start ss to install shadowsocks
+	print error bash ./rs.sh $1
 	"
 	exit 1
 fi
